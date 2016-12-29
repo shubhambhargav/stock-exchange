55027f64dada9e57c843852378b0db81b02dd9d8
shubhamb <shubham@greedygame.com>
2016-12-29 23:49:51 +0530

    default dirloc changed

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
+import os
 import sys
-sys.path.append('/Users/shubh/Documents/coding/practise/assignment/xamcheck')
+dirloc = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(dirloc)
 
 from argparse import ArgumentParser
 
